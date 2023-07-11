826aa3bb1cf4fe930a08e888f857866adb510189
Hit77Ro <smailettanjaoui@gmail.com>
2023-07-11 21:26:02 +0100

    "initial upload"

---
+++ dist/js/index.js
@@ -0,0 +1,28 @@
+function ShowMenu() {
+  let menu = document.querySelector(".header .nav");
+  let header = document.querySelector(".header");
+  let bar = menu.parentElement.querySelector(".menu-toggler");
+  bar.addEventListener("click", () => menu.classList.toggle("active"));
+
+  menu.addEventListener("click", (e) =>
+    e.target.matches(".nav-link") ? bar.click() : null
+  );
+  document.addEventListener("click", (e) =>
+    !header.contains(e.target) ? bar.click() : null
+  );
+}
+
+ShowMenu();
+// Start sticky header function
+function StickyHeader() {
+  let header = document.querySelector(".header");
+  header.querySelector(".nav").classList.contains(".active") ? header.querySelector(".nav").classList.remove("active") : null
+
+  window.addEventListener("scroll", function () {
+    let y = this.scrollY;
+    if (y > 700) header.classList.add("sticky");
+    else header.classList.remove("sticky");
+  });
+}
+
+StickyHeader();
